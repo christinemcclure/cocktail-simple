4989d891ef5881ae3add1fe88012edb5fb477204
Christine McClure <camcclure@gmail.com>
2012-09-24 19:03:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CocktailS::Application.config.secret_token = 'a8d3f55cf572282d741d240639cb8ed4fd9955ab5ef504838e488985ebc9088866e72944bb9a1f8746d9618588f7243e88e9f5e2bed3326fade6986fcecba517'
