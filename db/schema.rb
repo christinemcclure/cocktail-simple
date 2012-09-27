b926f62f69642996d8a18bb8a8dd97ba77600e41
Christine McClure <camcclure@gmail.com>
2012-09-26 21:21:58 -0500

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120925003124) do
+ActiveRecord::Schema.define(:version => 20120927021829) do
 
   create_table "cocktails", :force => true do |t|
     t.string   "name"
     t.text     "notes"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "ingredients", :force => true do |t|
+    t.string   "ingredient"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
