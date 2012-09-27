94498b06568445e42919081c4d05d01701e3b771
Christine McClure <camcclure@gmail.com>
2012-09-26 21:16:07 -0500

    removed dropped models, views and controllers for 'model' and 'ingredient'

--- app/models/ingredient.rb
+++
@@ -1,8 +0,0 @@
-class Ingredient < ActiveRecord::Base
-  attr_accessible :ingredient
-  has_one :cocktail, :dependent => :destroy
-end
-
-def down
-  drop_table :ingredient
-end
--- app/models/model.rb
+++
@@ -1,8 +0,0 @@
-class Model < ActiveRecord::Base
-  attr_accessible :name, :notes
-  has_many :ingredients
-end
-
-def down
-  drop_table :model
-end
