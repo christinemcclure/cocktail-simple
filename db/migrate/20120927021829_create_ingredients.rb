3b2b041547c9549a7152f733d2abdc9bd35dc585
Christine McClure <camcclure@gmail.com>
2012-09-30 15:07:49 -0500

    tried rollback of db

--- db/migrate/20120927021829_create_ingredients.rb
+++ db/migrate/20120927021829_create_ingredients.rb
@@ -2,7 +2,7 @@ class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
       t.string :ingredient
-
+      t.string :cocktail_id
       t.timestamps
     end
   end
